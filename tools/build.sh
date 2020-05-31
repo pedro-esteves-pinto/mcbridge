943aa517bad3cf513c5f0544d2a33dc0b6ba37ed
Pedro Pinto <pedro-esteves-pinto@users.noreply.github.com>
2020-05-30 22:00:51 -0400

    Tweaked build scripts.

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BUILD=${1:-debug}
-ninja -C `readlink -f $DIR/../build`/$BUILD
+(cd `readlink -f $DIR/../build`/$BUILD && ninja)
 
--- tools/setup_builds.sh
+++ tools/setup_builds.sh
@@ -8,7 +8,8 @@ mkdir -p $BUILD_ROOT
 
 rm -rf $BUILD_ROOT/debug
 mkdir -p $BUILD_ROOT/debug
-cd $BUILD_ROOT/debugcmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=clang++ $SRC_ROOT
+cd $BUILD_ROOT/debug
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=clang++ $SRC_ROOT
 
 rm -rf $BUILD_ROOT/release
 mkdir -p $BUILD_ROOT/release
