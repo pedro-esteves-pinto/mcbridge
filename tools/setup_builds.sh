01e800d228a43a1dda7da55e4bfe2f7a4e4ab461
Pedro Pinto <pedro-esteves-pinto@users.noreply.github.com>
2020-05-30 21:26:22 -0400

    Fixed gcc build.

--- tools/setup_builds.sh
+++ tools/setup_builds.sh
@@ -8,10 +8,19 @@ mkdir -p $BUILD_ROOT
 
 rm -rf $BUILD_ROOT/debug
 mkdir -p $BUILD_ROOT/debug
-cd $BUILD_ROOT/debug
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=clang++ $SRC_ROOT
+cd $BUILD_ROOT/debugcmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=clang++ $SRC_ROOT
 
 rm -rf $BUILD_ROOT/release
 mkdir -p $BUILD_ROOT/release
 cd $BUILD_ROOT/release
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=clang++ $SRC_ROOT
+
+rm -rf $BUILD_ROOT/debug_gcc
+mkdir -p $BUILD_ROOT/debug_gcc
+cd $BUILD_ROOT/debug_gcc
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=g++ $SRC_ROOT
+
+rm -rf $BUILD_ROOT/release_gcc
+mkdir -p $BUILD_ROOT/release_gcc
+cd $BUILD_ROOT/release_gcc
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_CXX_COMPILER=g++ $SRC_ROOT
