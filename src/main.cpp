5e2b2cc109f53985f6bf95599ce9153a607f36dd
Pedro Pinto <pedro-esteves-pinto@users.noreply.github.com>
2020-06-06 15:25:43 -0400

    Minor refactor.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 using namespace mcbridge;
 
-uint32_t resolve_interface_ip(std::string const&interface_name) {
+uint32_t resolve_interface_ip(std::string const &interface_name) {
    auto ip = get_interface_ip(interface_name);
    if (ip)
       return ip.value();
@@ -19,6 +19,20 @@ uint32_t resolve_interface_ip(std::string const&interface_name) {
       LOG(fatal) << "Invalid interface name: " << interface_name;
       exit(-1);
    }
+}
+
+std::set<EndPoint> read_groups_from_file(std::string const &groups_file) {
+   std::set<EndPoint> result;
+   std::ifstream f(groups_file);
+   while (!f.eof()) {
+      std::string ip_port;
+      f >> std::skipws >> ip_port;
+      if (ip_port.size()) {
+         EndPoint ep(ip_port);
+         result.insert(ep);
+      }
+   }
+   return result;
 }
 
 int main(int argc, char **argv) {
@@ -39,11 +53,12 @@ int main(int argc, char **argv) {
    client_cmd->add_option("-a", server_addr, "Server address")->required();
    std::string groups_file;
    client_cmd
-      ->add_option("-f", groups_file,
-                   "Read multicast groups to join from file")
-      ->check(CLI::ExistingFile);
+       ->add_option("-f", groups_file,
+                    "Read multicast groups to join from file")
+       ->check(CLI::ExistingFile);
    std::vector<std::string> groups;
-   client_cmd->add_option("-g", groups, "Multicast groups to join in the form ip:port");
+   client_cmd->add_option("-g", groups,
+                          "Multicast groups to join in the form ip:port");
    client_cmd->add_option("-p", port, "Server port", 30000);
    client_cmd->add_option("-i", interface,
                           "Interface to use for outbound multicast");
@@ -56,7 +71,7 @@ int main(int argc, char **argv) {
 
    // test_send
    auto test_send = app.add_subcommand(
-      "test_send", "Send a small multicast packet every few seconds");
+       "test_send", "Send a small multicast packet every few seconds");
    std::string group_s = "224.0.255.255:30000";
    test_send->add_option("-g", group_s, "Multicast group to send to",
                          "224.0.255.255:30000");
@@ -64,7 +79,7 @@ int main(int argc, char **argv) {
                          "Interface to use for outbound multicast");
 
    auto test_recv = app.add_subcommand(
-      "test_recv", "Receive multicast and print a few bytes to screen");
+       "test_recv", "Receive multicast and print a few bytes to screen");
    std::string group_c = "224.0.255.255:30000";
    test_recv->add_option("-g", group_c, "Multicast group to join",
                          "224.0.255.255:30000");
@@ -75,42 +90,32 @@ int main(int argc, char **argv) {
 
    set_log_level(logging_verbosity);
 
-
    if (*client_cmd) {
       ClientConfig cfg;
-      if (interface.size()) 
+      if (interface.size())
          cfg.outbound_interface = resolve_interface_ip(interface);
+      if (groups_file.size())
+         cfg.joined_groups = read_groups_from_file(groups_file);
       for (auto &g : groups)
          cfg.joined_groups.insert({g});
-      if (groups_file.size()) {
-         std::ifstream f(groups_file);
-         while (!f.eof()) {
-            std::string ip_port;
-            f >> std::skipws >> ip_port;
-            if (ip_port.size()) {
-               EndPoint ep(ip_port);
-               cfg.joined_groups.insert(ep);
-            }
-         }
-      }
       cfg.server_address = {resolve_host_name(server_addr), port};
       return Client{cfg}.run();
    } else if (*server_cmd) {
       ServerConfig cfg;
       cfg.port = port;
-      if (interface.size()) 
+      if (interface.size())
          cfg.inbound_interface = resolve_interface_ip(interface);
       return Server{cfg}.run();
    } else if (*test_send) {
       auto interface_ip = 0;
-      if (interface.size()) 
+      if (interface.size())
          interface_ip = resolve_interface_ip(interface);
-      return Test::send(group_s,interface_ip);
+      return Test::send(group_s, interface_ip);
    } else if (*test_recv) {
       auto interface_ip = 0;
-      if (interface.size()) 
+      if (interface.size())
          interface_ip = resolve_interface_ip(interface);
-      return Test::recv(group_c,interface_ip);
+      return Test::recv(group_c, interface_ip);
    }
 
    return -1;
