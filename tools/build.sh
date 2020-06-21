3262aaa6f7c231ff93e7e98d9c09d860585b5f06
Pedro Pinto <pedro-esteves-pinto@users.noreply.github.com>
2020-06-21 19:54:39 -0400

    Tweaked build tool.

--- tools/build.sh
+++ tools/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(dirname "$(readlink -f "$0")")"
 BUILD=${1:-debug}
 touch $DIR/../CMakeLists.txt
 (cd `readlink -f $DIR/../build`/$BUILD && ninja)
